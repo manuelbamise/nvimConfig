c18d742599cc4770ad70ee48c0431fd6564d8b10
manuelbamise <etobi1930@gmail.com>
2025-04-07 12:53:55 +0100

    process: initial commit

---
+++ lua/chadrc.lua
@@ -0,0 +1,40 @@
+-- This file needs to have same structure as nvconfig.lua
+-- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
+-- Please read that file to know all available options :(
+
+---@type ChadrcConfig
+local M = {}
+
+M.base46 = {
+  theme = "nightowl",
+
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
+}
+
+M.nvdash = { load_on_startup = true }
+M.ui = {
+  statusline = {
+    theme = "default",
+    separator_style = "arrow",
+    order = { "mode", "file", "git", "%=", "lsp_msg", "%=", "lsp", "cwd", "xyz" },
+    modules = {
+      file="%f",
+      xyz = function()
+        return "hello world"
+      end,
+    },
+  },
+
+  tabufline = {
+    lazyload = false,
+  },
+  vim.opt.foldmethod == "expr",
+  vim.opt.foldexpr == "nvim_treesitter#foldexpr()",
+  vim.opt.foldenable == true,
+  vim.opt.foldlevel == 99,
+}
+
+return M
---
+++ lua/configs/lspconfig.lua
@@ -0,0 +1,46 @@
+-- load defaults i.e lua_lsp
+require("nvchad.configs.lspconfig").defaults()
+
+local lspconfig = require "lspconfig"
+
+-- EXAMPLE
+local servers = { "html", "cssls" }
+local nvlsp = require "nvchad.configs.lspconfig"
+
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+  }
+end
+
+lspconfig.volar.setup {
+  filetypes = { "typescript", "vue", "javascript", "javascriptreact", "typescriptreact" },
+}
+lspconfig.tailwindcss.setup {
+  filetypes = { "html", "vue", "typescriptreact", "javascriptreact", "javascript", "typescript" },
+  init_options = {
+    userLanguages = {
+      vue = "html",
+    },
+  },
+}
+
+lspconfig.emmet_ls.setup({
+        filetypes = { "html", "css", "vue", "javascriptreact", "typescriptreact" },
+        init_options = {
+          html = {
+            options = {
+              ["bem.enabled"] = true,
+            },
+          },
+        },
+      })
+-- configuring single server, example: typescript
+-- lspconfig.ts_ls.setup {
+--   on_attach = nvlsp.on_attach,
+--   on_init = nvlsp.on_init,
+--   capabilities = nvlsp.capabilities,
+-- }
