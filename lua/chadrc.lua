2b3dfc6a8d28ffe3d3f814478cb1bf2c22a0e6b1
manuelbamise <etobi1930@gmail.com>
2025-04-10 05:38:56 +0100

    process: tried to add relative number to be the default setting

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -35,6 +35,8 @@ M.ui = {
   vim.opt.foldexpr == "nvim_treesitter#foldexpr()",
   vim.opt.foldenable == true,
   vim.opt.foldlevel == 99,
+  vim.opt.number == true,
+  vim.opt.relativenumber == true,
 }
 
 return M
